eb05eac3f79d5cfdf251d3145f00b51258815285
Sebastien Dubois <sdubois@protonmail.com>
2016-03-21 17:04:30 +0200

    Add some debug logs for eventBus messages

--- src/main/java/com/github/sedubois/practice/service/PracticeService.java
+++ src/main/java/com/github/sedubois/practice/service/PracticeService.java
@@ -2,8 +2,6 @@ package com.github.sedubois.practice.service;
 
 import com.github.sedubois.practice.Practice;
 
-import java.util.Collection;
-
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -20,60 +18,62 @@ public class PracticeService {
 
   public Practice update(Practice practice) {
     Vertx vertx = Vertx.currentContext().owner();
-    
+
     if (this.practice == null) {
       this.practice = new Practice(practice.getTotalSeconds());
     }
-    
+
     if (this.practice.getTimerId() != null) {
       vertx.cancelTimer(this.practice.getTimerId());
     }
-    
+
     if (practice.getTotalSeconds() != null) {
       setTotalSeconds(practice.getTotalSeconds());
     }
-    
+
     if (practice.getRemainingSeconds() != null) {
       setRemainingSeconds(practice.getRemainingSeconds());
     }
-    
+
     if (practice.isStarted() != null) {
       setStarted(practice.isStarted());
     }
-    
+
     doPractice();
     return practice;
   }
-  
+
   private void doPractice() {
     Vertx vertx = Vertx.currentContext().owner();
-    
+
     if (this.practice.isStarted()) {
       this.practice.setTimerId(vertx.setPeriodic(1000, id -> {
-        this.practice.setRemainingSeconds(this.practice.getRemainingSeconds() - 1);
-        vertx.eventBus().publish("timer.remainingSeconds", practice.getRemainingSeconds());
-      
+        setRemainingSeconds(this.practice.getRemainingSeconds() - 1);
+
         if(practice.getRemainingSeconds() == 0) {
           vertx.cancelTimer(this.practice.getTimerId());
-          vertx.eventBus().publish("timer.started", false);
+          setStarted(false);
           this.practice = null;
         }
       }));
     }
   }
-  
+
   private void setTotalSeconds(long totalSeconds) {
     this.practice.setTotalSeconds(totalSeconds);
+    System.out.println("Publishing timer.totalSeconds=" + totalSeconds + " to eventBus");
     Vertx.currentContext().owner().eventBus().publish("timer.totalSeconds", totalSeconds);
   }
-  
+
   private void setRemainingSeconds(long remainingSeconds) {
     this.practice.setRemainingSeconds(remainingSeconds);
+    System.out.println("Publishing timer.remainingSeconds=" + remainingSeconds + " to eventBus");
     Vertx.currentContext().owner().eventBus().publish("timer.remainingSeconds", remainingSeconds);
   }
-  
+
   private void setStarted(boolean started) {
     this.practice.setStarted(started);
+    System.out.println("Publishing timer.started=" + started + " to eventBus");
     Vertx.currentContext().owner().eventBus().publish("timer.started", started);
   }
 }
